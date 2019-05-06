9e900b83c6304e991f0ab270558f2d94d70ed8b4
Ignacio <imoon521@gmail.com>
2019-05-05 20:14:39 -0600

    Adding new attributes to Income Model, Add a  in case of fail when try to save, add  to sails project to get  dates

--- api/controllers/IncomeController.js
+++ api/controllers/IncomeController.js
@@ -4,6 +4,7 @@
  * @description :: Server-side actions for handling incoming requests.
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
+const moment = require("moment");
 
 module.exports = {
   find: async (req, res) => {
@@ -11,9 +12,29 @@ module.exports = {
     res.ok(incomes);
   },
   create: async (req, res) => {
+    // Getting data from request
     let name = req.param("name");
+    let UID = req.param("UID");
+    let description = req.param("description");
     let amount = req.param("amount");
-    let income = await Income.create({ name, amount }).fetch();
-    res.ok(income);
+    let category = req.param("category");
+    let date = req.param("date") || moment().unix();
+
+    // Save or return 422
+    try {
+      let income = await Income.create({
+        name,
+        description,
+        amount,
+        category,
+        date,
+        UID
+      }).fetch();
+
+      res.ok(income);
+    } catch (error) {
+      res.status(422);
+      res.send({ error: error.details });
+    }
   }
 };
