7b1d4387a0930c5328d7fb67065d6c9c5326dc26
Ignacio <imoon521@gmail.com>
2019-05-13 20:01:34 -0600

    Cleaning base app

--- client/src/containers/DefaultLayout/DefaultFooter.js
+++ client/src/containers/DefaultLayout/DefaultFooter.js
@@ -1,22 +1,23 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 const propTypes = {
-  children: PropTypes.node,
+  children: PropTypes.node
 };
 
 const defaultProps = {};
 
 class DefaultFooter extends Component {
   render() {
-
     // eslint-disable-next-line
     const { children, ...attributes } = this.props;
 
     return (
       <React.Fragment>
-        <span><a href="https://coreui.io">CoreUI</a> &copy; 2018 creativeLabs.</span>
-        <span className="ml-auto">Powered by <a href="https://coreui.io/react">CoreUI for React</a></span>
+        <span>
+          <a href="https://github.com/PotatoCodeOfficial">Potato Code</a> &copy;
+          2019.
+        </span>
       </React.Fragment>
     );
   }
--- client/src/containers/DefaultLayout/DefaultLayout.js
+++ client/src/containers/DefaultLayout/DefaultLayout.js
@@ -3,7 +3,6 @@ import { Redirect, Route, Switch } from "react-router-dom";
 import { Container } from "reactstrap";
 
 import {
-  AppAside,
   AppBreadcrumb,
   AppFooter,
   AppHeader,
