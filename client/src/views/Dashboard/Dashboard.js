43ebd1dee833688a8e9eeb32862ae35ecf0529b0
Ignacio <imoon521@gmail.com>
2019-05-14 19:30:15 -0600

    Cleaning

--- client/src/views/Dashboard/Dashboard.js
+++ client/src/views/Dashboard/Dashboard.js
@@ -1,35 +1,5 @@
 import React, { Component, lazy, Suspense } from "react";
-import { Bar, Line } from "react-chartjs-2";
-import {
-  Badge,
-  Button,
-  ButtonDropdown,
-  ButtonGroup,
-  ButtonToolbar,
-  Card,
-  CardBody,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-  Col,
-  Dropdown,
-  DropdownItem,
-  DropdownMenu,
-  DropdownToggle,
-  Progress,
-  Row,
-  Table
-} from "reactstrap";
-import { CustomTooltips } from "@coreui/coreui-plugin-chartjs-custom-tooltips";
-import { getStyle, hexToRgba } from "@coreui/coreui/dist/js/coreui-utilities";
-
-const Widget03 = lazy(() => import("../../views/Widgets/Widget03"));
-
-const brandPrimary = getStyle("--primary");
-const brandSuccess = getStyle("--success");
-const brandInfo = getStyle("--info");
-const brandWarning = getStyle("--warning");
-const brandDanger = getStyle("--danger");
+import { getStyle } from "@coreui/coreui/dist/js/coreui-utilities";
 
 class Dashboard extends Component {
   constructor(props) {
