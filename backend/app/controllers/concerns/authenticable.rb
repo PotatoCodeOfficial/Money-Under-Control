b9a47ebc1d95d08551f84394e27eea6ee67702fe
Ignacio <imoon521@gmail.com>
2019-06-08 21:11:17 -0600

    Add a validation on user_id get

--- backend/app/controllers/concerns/authenticable.rb
+++ backend/app/controllers/concerns/authenticable.rb
@@ -2,8 +2,13 @@ module Authenticable
 include ActiveSupport::Concern
   def get_user_id
     # Use firebase gem to verify that the token is valid
+    return render_401 unless request.headers["Authorization"]
     token = request.headers["Authorization"].split("Bearer ")[1]
     decoded = JWT.decode(token, nil, false, {algorithm:  'HS256'})[0]
     @user_id = decoded["user_id"]
+  end
+
+  def render_401
+    render status: 401, json: {}
   end
 end
